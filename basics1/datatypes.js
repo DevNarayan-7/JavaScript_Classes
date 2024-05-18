fb238c265660bb0955e749e5906035e791521233
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2024-05-18 12:46:39 +0000

    discussed about datatypes

---
+++ basics1/datatypes.js
@@ -0,0 +1,54 @@
+//Primitive (STACK)
+
+/*
+String
+Number
+Boolean
+Null
+Undefined
+Symbol
+BigInt
+*/
+
+const name = "Alien"
+let age = 23
+let isLoggedIn = false
+const nodeVersion = null
+let bankBalance = undefined
+const buttonId = Symbol("Id")
+const aReallyLongNumber = 998655444321n
+
+
+//Reference (Non primitive) --Object (HEAP)
+
+/*
+
+Arrays
+Objects
+Functions
+
+*/
+
+
+let number = [1,2,3,4,5]
+let heroes = ["SuperMan, SpiderMan","BatMan"]
+
+
+let userObject = {
+    name: "Alien",
+    age : 25,
+    isLoggedIn :True
+}
+
+
+
+
+const sayHello = function(){
+    console.log("Hi Alien");
+}
+
+const registerUser = function(){
+}
+
+const logInUser = function(){}
+
