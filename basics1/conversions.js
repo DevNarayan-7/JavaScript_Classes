6a1d5c75eb06aea5c1a8362a16ffdf41958840cf
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2024-05-19 18:22:48 +0000

    discussed on conversions

---
+++ basics1/conversions.js
@@ -0,0 +1,21 @@
+//conversions
+//1. Implicit conversion  2. Explicit conversion
+
+
+let bankBalance = '1122'
+let bankBalanceInInt1 = parseInt(bankBalance)
+let bankBalanceInInt2 = Number(bankBalance)
+
+
+console.log(typeof bankBalance);
+console.log(typeof bankBalanceInInt1);
+console.log(typeof bankBalanceInInt2);
+
+
+let c = null
+
+
+console.log(5 + undefined);
+console.log(NaN === NaN);
+console.log(c)
+console.log(d)
