f9267921b072decd7f79c24ea914efa3ba56ec00
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2024-05-19 12:41:35 +0000

    discussed on Operators

---
+++ basics1/operators.js
@@ -0,0 +1,27 @@
+// Arithmetic Operators
+
+// 2+2
+// 2-2
+// 2*3 = 2*2*2
+// 2/2 
+// 2%2
+
+//Assignment Operators
+
+let valueOne = 10
+valueOne += 20
+
+console.log(valueOne)
+
+//Comparision Operator
+
+// Equal (==):
+// Strict Equal (===):
+
+let numOne = 3
+let numTwo = "3"
+
+console.log(numOne == numTwo); // true
+console.log(numOne === numTwo)// false as it is comparing data type also
+
+
