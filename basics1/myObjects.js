4eae81526799dfb78bd028d72f1660ed549f9df1
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2024-05-25 13:34:34 +0000

    dusccesed on Object

---
+++ basics1/myObjects.js
@@ -0,0 +1,39 @@
+const user = {
+    username : "Ali",
+    name : "AlienSpace",
+    email : "alien123@gmail.com",
+    age : 25,
+    isAdmin : false,
+    isLoggedIn : true,
+    tea : ["lemon", "peach", "ginger"],
+    address : {
+        city : "Hyderabad",
+        state : "Telangana"
+    }
+}
+
+
+const anotherUser = user
+//2 ways of accessing Object
+// console.log(user.age);
+// console.log(user['age']);
+// console.log(user.tea[1])
+// console.log(user.address.state);
+// console.log(user.address['state']);
+// console.log(user['address']['state']);
+
+
+user.email = "ali123@gmail.com"
+// console.log(user)
+// console.log(anotherUser)
+
+delete user.isAdmin
+// console.log(user)
+// console.log(anotherUser)
+
+console.log(Object.keys(user))
+console.log(Object.values(user))
+console.log(Object.entries(user))
+
+console.log(Object.keywords);
+console.log(user.hasOwnProperty('tea'));
