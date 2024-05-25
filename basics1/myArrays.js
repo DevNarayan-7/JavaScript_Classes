347868135cea1c88336a7f64b8ad63518dd41623
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2024-05-25 12:27:01 +0000

    discussed on array

---
+++ basics1/myArrays.js
@@ -0,0 +1,54 @@
+//Arrays
+
+// const myArray  = [1,2,3,4,5]
+
+// const myNewArray = new Array(1,2,3,4,5)
+
+// //console.log(myArray[1])
+
+// const dcHeroes = ["batman", "superman", "flash"]
+// //console.log(dcHeroes.length);
+
+// //dcHeroes[dcHeroes.length] = "wonder women"
+
+// dcHeroes.push("wonder women")
+// // console.log(dcHeroes)
+// // const lastHero = dcHeroes.pop()
+// // //costly one
+// // dcHeroes.unshift("aquman")
+
+// // console.log(dcHeroes.toString())
+
+// console.log(dcHeroes.slice(1,3))
+// console.log(dcHeroes)
+// console.log(dcHeroes.splice(1,3))
+// console.log(dcHeroes);
+
+
+
+
+
+
+
+// console.log(dcHeroes.join("-"))
+// const arr = new Array(1,2,3,4,5,6,7,8)
+
+// console.log(arr.slice(0,2));
+// console.log(arr.splice(0,2))
+
+
+////////////////////
+
+const arr = ["batman", "superman", "flash"]
+
+console.log(arr.push("wonder women"))
+
+console.log(arr)
+
+console.log(arr.slice(1,3))
+
+console.log(arr)
+
+console.log(arr.splice(1,3))
+
+console.log(arr);
