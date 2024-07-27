50b93e5ac0f3156415e167624c16eb7deda786f0
DevNarayan-7 <143680777+DevNarayan-7@users.noreply.github.com>
2024-07-27 11:15:11 +0000

    discussed on types of Functions

---
+++ functions2/anonymous.js
@@ -0,0 +1,6 @@
+const evenOrodd = function(num){
+    return num % 2 == 0;
+}
+
+let result  = evenOrodd(2);
+console.log(result);
---
+++ functions2/what_is_this.js
@@ -0,0 +1,29 @@
+function fun(){  // function declaration
+
+}
+
+//function expression
+
+const f = function process(){ // name function expression
+
+}
+
+const g = function(){ // anonymous function
+
+}
+
+const h = () => {
+
+}
+
+
+// IIFE(Immediatly invoke function expression )
+
+(function(){
+
+})()
+
+
+(function exec(){
+
+})()
